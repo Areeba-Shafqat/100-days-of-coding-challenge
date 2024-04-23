a80d01ab7cce36dc20d2dc91987da6b6b2326db6
Areeba-Shafqat <162574289+Areeba-Shafqat@users.noreply.github.com>
2024-04-23 16:08:57 -0700

    Five days challenge has been completed successfully.

---
+++ day-04/q10/main.ts
@@ -0,0 +1,8 @@
+// Question 10: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.
+
+
+// Areeba, 23-04-2024
+let poetName : string = "Allama Iqbal";// stores favourite poet name
+let leader : string = "Quid-e-Azam"; // stores leader name
+
+console.log(`${poetName} gave the idea of Pakistan and ${leader} was the foounder of Pakistan.`); // shows message why they are favourite personilties
