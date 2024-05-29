230558013c57acb57a67e8e0b9a11687f936f40f
Areeba-Shafqat <162574289+Areeba-Shafqat@users.noreply.github.com>
2024-05-29 17:54:21 +0500

    Ten days challenge has been completed successfully.

--- day01-05/day-02/q6/main.js
+++ day01-05/day-02/q6/main.js
@@ -1,4 +1,4 @@
 // Question 3: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
-var myName = "\t\n Areeba \t\n"; //store's the name with whitespace
+var myName = "\n\t Areeba \t\n"; //store's the name with whitespace
 console.log(myName); // shows the name with whitespace
 console.log(myName.trim()); // shows the name without whitespace
