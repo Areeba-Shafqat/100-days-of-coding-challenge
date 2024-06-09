dca53c6a6421470f1b70f9e077957ff947270eb9
Areeba-Shafqat <162574289+Areeba-Shafqat@users.noreply.github.com>
2024-06-09 11:18:56 +0500

    Five days challenge has been completed successfully.

--- day06-10/day-07/q21/main.js
+++
@@ -1,8 +0,0 @@
-// 21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
-var ansSheet = {
-    name: "Areeba",
-    fName: "Muhammad Shafqat",
-    rollNumber: 10,
-    subject: "Math",
-};
-console.log(ansSheet);
