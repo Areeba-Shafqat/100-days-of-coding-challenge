e583aeb62867b9ea49615664fb990668b608e2a8
Areeba-Shafqat <162574289+Areeba-Shafqat@users.noreply.github.com>
2024-06-09 11:22:52 +0500

    Five days challenge has been completed successfully.

---
+++ day01-05/day-01/q2/main.ts
@@ -0,0 +1,4 @@
+// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+
+let personName: string = "Areeba"; // store a person name 
+console.log(`Hello ${personName}, would you like to learn some TypeScript today?`);// shows a message to them
---
+++ day01-05/day-01/q3/main.ts
@@ -0,0 +1,10 @@
+// Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName: string ="areeba";// store a person name 
+
+console.log(personName.toLowerCase());// shows the name in all small letters
+
+console.log(personName.toUpperCase());//shows the name in all big letters
+
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase()); //shows the name's First letter Big and the rest of all in small letters
+
