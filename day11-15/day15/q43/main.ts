e7d9e23b99d004e65d7c7d019d02f9b01e8fdd0b
Areeba-Shafqat <162574289+Areeba-Shafqat@users.noreply.github.com>
2024-06-09 11:49:31 +0500

    Fifteen days challenge has been completed successfully.

--- day11-15/day15/q43/main.ts
+++ day11-15/day15/q43/main.ts
@@ -1,4 +1,4 @@
-// 43. Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
+// 43. Unchanged Magicians: Preserve the original magician names while creating a new "great" list.
 
 let magician_name: string[] = ["Ali", "Zeeshan", "Faisal", "Hasan"];
 function make_great(megician_name: string[]) {
