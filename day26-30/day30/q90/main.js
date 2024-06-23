7a9c443de32814bd969743f241d7f3700ae43f76
Areeba-Shafqat <162574289+Areeba-Shafqat@users.noreply.github.com>
2024-06-23 13:29:37 +0500

    Thirty days challenge has been completed successfully.

---
+++ day26-30/day30/q90/main.js
@@ -0,0 +1,6 @@
+// Question 90: Checking if a Value is NaN: Demonstrate how to check if a variable's value is NaN (Not a Number) and return a boolean result.
+function Checking(value) {
+    return isNaN(value);
+}
+console.log(Checking(87)); // Outputs: false, because 123 is a number
+console.log(Checking("Good Morning!")); // Outputs: true, because "Good Morning!" isn't a number
